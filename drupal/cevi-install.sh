cc0ecbcc8482075692683432763c757572efa46e
btemperli <beat@temper.li>
2021-01-14 19:00:07 +0100

    auto-yes on installation

--- drupal/cevi-install.sh
+++ drupal/cevi-install.sh
@@ -2,7 +2,7 @@
 
 composer install
 
-./vendor/bin/drush si --locale=de
+./vendor/bin/drush si --locale=de -y
 
 ./vendor/bin/drush config-set system.site uuid eb76eb7c-70c3-4296-960b-673b5f7702af -y
 ./vendor/bin/drush config-set language.entity.de uuid 7e2040a3-be26-44ad-ba1e-c5902dded730 -y
