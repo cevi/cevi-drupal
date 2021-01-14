e6276b8dd7e39beedbb3376c3fe24cff678eb75b
btemperli <beat@temper.li>
2021-01-14 19:19:23 +0100

    update shortcut uuid

--- drupal/cevi-install.sh
+++ drupal/cevi-install.sh
@@ -6,6 +6,6 @@ composer install
 
 ./vendor/bin/drush config-set system.site uuid eb76eb7c-70c3-4296-960b-673b5f7702af -y
 ./vendor/bin/drush config-set language.entity.de uuid 7e2040a3-be26-44ad-ba1e-c5902dded730 -y
-./vendor/bin/drush config-set shortcut.set.default uuid null -y
+./vendor/bin/drush config-set shortcut.set.default uuid 6e0504ef-46e5-42bf-a1a2-cd097c1689ce -y
 
 ./vendor/bin/drush cim -y
